a678436e81c34f26736529fc1d72bade388f1702
RomainDoukhan <89915670+RomainDoukhan@users.noreply.github.com>
2021-10-18 10:35:02 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ]
+    "Romain_Doukhan" => [
+        "city" => "Paris",
+        "weapon" => "Knife"
+    ]
+];
+
+var_dump($characters);
+
+?>
